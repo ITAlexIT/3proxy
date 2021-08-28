a8f082952b144a8d22d64704ee899fba16e3d960
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-28 17:30:52 +0300

    Create ufw.sh

---
+++ ufw.sh
@@ -0,0 +1,3 @@
+cd /etc/ufw/
+wget --no-check-certificate https://github.com/ITAlexIT/3proxy/raw/master/user.rules
+wget --no-check-certificate https://github.com/ITAlexIT/3proxy/raw/master/user6.rules
