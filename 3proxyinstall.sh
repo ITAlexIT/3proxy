a33842d53ab554ea257cd61e0b4e843c5843c8be
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-28 19:44:22 +0300

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -29,7 +29,15 @@ wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user.rules
 wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user.rules
 wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user6.rules
 wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/before.rules
-cp -f user.rules /etc/ufw/
-cp -f user6.rules /etc/ufw/
-cp -f before.rules /etc/ufw/
+cp -f /etc/3proxy/user.rules /etc/ufw/
+cp -f /etc/3proxy/user6.rules /etc/ufw/
+cp -f /etc/3proxy/before.rules /etc/ufw/
+ufw disable && ufw enable
+expect <<END
+        set timeout 3
+        spawn ufw disable && ufw enable
+        expect "Command may disrupt existing ssh connections. Proceed with operation"
+        send -- "y\r"
+        expect eof
+END
 /etc/init.d/3proxy start
