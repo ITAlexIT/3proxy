702b31be21c6f14e8b73f35cef88fdd8cd121509
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-28 18:00:01 +0300

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,3 +1,4 @@
-cd /etc/ufw/
-wget --no-check-certificate https://raw.github.com/ITAlexIT/3proxy/master/user.rules
-wget --no-check-certificate https://raw.github.com/ITAlexIT/3proxy/master/user6.rules
+wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user.rules
+wget P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user6.rules
+cp -f user.rules /etc/ufw/
+cp -f user6.rules /etc/ufw/
