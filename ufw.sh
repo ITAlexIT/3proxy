121ee18c358e06e327c11fbb58dbf5eec41b1dc1
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-28 18:08:15 +0300

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,4 +1,5 @@
 wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user.rules
 wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user6.rules
+wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/before.rules
 cp -f user.rules /etc/ufw/
 cp -f user6.rules /etc/ufw/
