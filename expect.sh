1026532f677ca1045ec49f9b36e2fbfff621d3a1
ITAlexIT <60471197+ITAlexIT@users.noreply.github.com>
2021-08-28 19:18:06 +0300

    Update expect.sh

--- expect.sh
+++ expect.sh
@@ -4,3 +4,9 @@ spawn ufw enable
 expect "Command may disrupt existing ssh connections. Proceed with operation"
 send -- "y\r"
 expect eof
+wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user.rules
+wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/user6.rules
+wget -P /etc/3proxy/ https://raw.github.com/ITAlexIT/3proxy/master/before.rules
+cp -f user.rules /etc/ufw/
+cp -f user6.rules /etc/ufw/
+cp -f before.rules /etc/ufw/
